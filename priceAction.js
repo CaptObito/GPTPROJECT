83eb5f9c515e494376d74af73e6b8f617f4438ea
CaptObito <96685873+CaptObito@users.noreply.github.com>
2025-03-23 14:38:31 +0700

    Delete priceAction.js
    delete diles

--- priceAction.js
+++
@@ -1,6 +0,0 @@
-const socket = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
-
-socket.onmessage = function(event) {
-    const data = JSON.parse(event.data);
-    document.getElementById('price').innerText = `$${parseFloat(data.p).toFixed(2)}`;
-};
